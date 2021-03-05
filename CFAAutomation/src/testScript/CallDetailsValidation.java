f6044d0bdf11d7527589948ce75ff3db500359d4
Snehal <snehal.d@moentek.com>
2021-03-05 09:36:05 +0530

    Added new function Test2() to login

--- CFAAutomation/src/testScript/CallDetailsValidation.java
+++ CFAAutomation/src/testScript/CallDetailsValidation.java
@@ -1,5 +1,6 @@
 package testScript;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
@@ -8,13 +9,31 @@ import functionLibrary.ApplicationLibrary;
 
 public class CallDetailsValidation {
 	
+	WebDriver dr;
+	
 	@Test
 	public void test1()
 	{
 		ApplicationLibrary lib= new ApplicationLibrary();
 		
-		WebDriver dr=lib.launchBrowser("Chrome");
+		dr=lib.launchBrowser("Chrome");
 		dr.get(Configuration.url);
+		
 	}
+	
+	@Test
+	public void test2()
+	{
+		
+		dr.findElement(By.id("email")).sendKeys("snehal.d@moentek.com");
+		
+		dr.findElement(By.id("password")).sendKeys("lmc2demo");
+		
+		dr.findElement(By.id("b1")).click();
+		
+		
+	}
+	
+	
 
 }
